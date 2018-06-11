fc06131073506157217d2f0c759b4913ade4505c
Jordan Jolly <jordanwjolly@gmail.com>
2018-06-11 05:32:14 +0000

    cleaned up directory

--- Interface/actions/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once('post_action.php');
-	
-	
-?>
--- Interface/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once('config.php');
-	$db = db_connect();
-	clear_auth($db);
-	$db->close();
-	
-	header('Location: '.ROOT);
-?>
