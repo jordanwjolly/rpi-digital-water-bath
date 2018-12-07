8b96e7ff3da3616e0080ef0a33300e621e3bc621
Jordan Jolly <jordanwjolly@gmail.com>
2018-12-07 14:39:02 +1100

    FULLY WORKING SYSTEM. WTI_setup function (all starting off). Fixed bug in heater on/off logic. Increased WTI shell script delay by 1 sec

---
+++ Hardware_control/Temp_sensor_OLD.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+import os
+import glob
+import time
+import pickle
+import numpy as np
+
+#CHANGE THIS
+s1="28-000006dc6863"
+s2="28-000006dc76f3"
+s3=None
+s4=None
+s5=None
+s6=None
+s7=None
+s8=None
+
+sensorID=[s1, s2, s3, s4, s5, s6, s7, s8] #Sensor 1 for tank 1, is in the first place etc...
+
+#DONT CHANGE ANYTHING ELSE
+sensorVAL = np.array([0,0,0,0,0,0,0,0], dtype=np.float) #Initiates an 1x8 array of NaNs
+sensorVAL.fill(np.nan)
+
+
+os.system('modprobe w1-gpio');
+os.system('modprobe w1-therm');
+
+READ_DELAY = 0.1;
+AVERAGE_COUNT = 3;
+
+BASE_DIR = '/sys/bus/w1/devices/';
+
+#Save path of current temperature
+DIR = dir_path = os.path.dirname(os.path.realpath(__file__))
+CURRENT_FILE = DIR+'/SensorValues.txt'
+
+def get_device_file(sensorID):
+	global BASE_DIR;
+	file = None;
+	try:
+		file = BASE_DIR + sensorID + '/w1_slave';
+	except:
+		pass;
+	return file;
+
+def read_temp_raw(device_file):
+	lines = None;
+	with open(device_file, 'r') as file:
+		lines = file.readlines();
+	return lines;
+
+def read_temp(device_file):
+	lines = None;
+	equals_pos = -1;
+	while equals_pos < 0 or lines[0].strip()[-3:] != 'YES':
+		time.sleep(0.15);
+		lines = read_temp_raw(device_file);
+		equals_pos = lines[1].find('t=');
+	temp_string = lines[1][equals_pos+2:];
+	temp_c = float(temp_string)/1000.0;
+	return (temp_c);
+
+def get_average_temp(device_file):
+	global AVERAGE_COUNT
+	
+	total_c = 0
+	for i in range(0,AVERAGE_COUNT):
+		temp_c = read_temp(device_file)
+		total_c += temp_c
+	return total_c/AVERAGE_COUNT
+
+def write_current(temp):
+	global CURRENT_FILE
+	
+	
+
+####MAIN CODE#########
+
+# First try to get the device file
+print('Finding sensor device file...\n');
+
+while(True):
+	
+	print("\n#######################################################")
+	for index, sensor in enumerate(sensorID):
+	
+		device_file = get_device_file(sensor);
+
+		avg_temp = -1;
+		try:
+			avg_temp = get_average_temp(device_file);
+		except KeyboardInterrupt:
+			break;
+		except Exception as e:
+			print('Error getting temp reading from SENSOR ' + str(index)+': '+str(e));
+			continue;
+	
+
+		print('\nCurrent Temp of SENSOR '+str(index+1) +', sensor ID: ' + sensor)
+		print('({:d}) {:1.3f} C'.format(int(time.time()), avg_temp));
+		sensorVAL[index]=avg_temp			
+		
+	pickle.dump(sensorVAL, open(CURRENT_FILE, 'w'))
+	
+	time.sleep(READ_DELAY);
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This is a basic bang-bang temperature controller
+SUMMARY
+
+This is a basic bang-bang temperature controller used for maintaining desired tempreatures in up to 8 seperate aquatic environments simultaneously.
 It can be used to both heat, and cool an environment based off a desired temperature profile curve.
 
 The system uses the following sensors/actuators:
@@ -6,11 +8,47 @@ The system uses the following sensors/actuators:
 	- Waterproof DS18B20 Digital temperature sensor for environmental sensing
 	- Heating element
 	- Cooling element
+	- Network Power Switch from WTI
 
-The various scripts are all launched by runnning launch.sh
+Where multiple water sources are required to be heated/cooled simatenously, a single cooler, heater, and tempreature sensor will be required per tank. 
+A single setwork power switch is all thats required.
 
-A user can toggle a gui graph of temperature histroy turning on/off
-The desired curve can be edited by changing the equation in temperature_profile.py
+User options:
+
+- A user can toggle a gui graph of temperature history turning on/off
+- The desired curve can be edited by changing the equation in temperature_profile.py
+
+GENERAL USER USE
+
+Temperature sensors:
+As the DS18B20 temperature sensor is a 1-wire device, as many of these sensors can all be connected to the same pull-up resistor circuirty
+It is best if a user connects each of the sensors once at a time, and notes the unique sensor ID address.
+
+User navigates to here:
+
+cd /sys/bus/w1/devices/
+
+Notes down the sensor ID, physically labels the sensor with a number 1-8, and then copies the sensor ID to the correspeonding slot in Temp_sensor.py
 
 
+User specified temp:
+User inputs the temprature_profile.py script
 
+
+TO GET SOFTWARE RUNNING
+
+Open up a terminal on the raspberry pi.
+
+change directories "cd /RaspberryPiThermostat/Hardware_control"
+
+run "python Temp_sensor.py"
+
+
+....your temprature sensors are now running!!
+
+change directories "cd /RaspberryPiThermostat/"
+
+run "python temperature_profile.py"
+
+
+...everything is now running!!!!!!!!!!!!!!!
